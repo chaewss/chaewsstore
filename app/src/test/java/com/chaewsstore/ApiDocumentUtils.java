547ec514e18d09b29626b1b1fa351485fe4b77c8
chaewss <chaewseo12@gmail.com>
2023-02-21 00:54:17 +0900

    docs: 회원가입 api 문서 작성 #4

---
+++ app/src/test/java/com/chaewsstore/ApiDocumentUtils.java
@@ -0,0 +1,26 @@
+package com.chaewsstore;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+public interface ApiDocumentUtils {
+
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(
+            modifyUris()
+                .scheme("https")
+                .host("chaewsstore.docs.api.com")
+                .port(8000),
+            prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+
+}
