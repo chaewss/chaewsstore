d0faafcf06e0a95c9ed892a85a23343a04cc69df
chaewss <chaewseo12@gmail.com>
2024-05-15 19:34:42 +0900

    chore: security 디버깅 로그 옵션 삭제

--- app/src/main/java/com/chaewsstore/config/SecurityConfig.java
+++ app/src/main/java/com/chaewsstore/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
