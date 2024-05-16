012d74cf559d4ca2118d1e28912e5ab0a93d1164
chaewss <chaewseo12@gmail.com>
2024-05-17 04:03:29 +0900

    chore: unique 제약조건 추가 #20

--- core/src/main/java/com/chaewsstore/entity/Product.java
+++ core/src/main/java/com/chaewsstore/entity/Product.java
@@ -1,5 +1,6 @@
 package com.chaewsstore.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Product extends BaseTimeEntity {
     private Long id;
 
     @NotNull
+    @Column(unique = true)
     private String name;
 
     @NotNull
