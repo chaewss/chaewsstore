41d3e8d01e460432717f78d35bc5528dba9eb4f9
chaewss <chaewseo12@gmail.com>
2024-05-31 04:02:06 +0900

    chore: 불필요한 인터페이스 삭제

--- chaewsstore-app/src/main/java/com/chaewsstore/common/security/handler/JwtAccessDeniedHandler.java
+++ chaewsstore-app/src/main/java/com/chaewsstore/common/security/handler/JwtAccessDeniedHandler.java
@@ -4,7 +4,6 @@ import static com.chaewsstore.core.common.util.ResponseCode.ACCESS_DENIED;
 import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-import com.chaewsstore.core.common.auth.FilterExceptionResolver;
 import com.chaewsstore.core.common.util.ResponseData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,24 +14,18 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @RequiredArgsConstructor
-public class JwtAccessDeniedHandler implements AccessDeniedHandler,
-    FilterExceptionResolver<AccessDeniedException> {
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
         AccessDeniedException accessDeniedException) throws IOException {
-        setResponse(response, accessDeniedException);
-    }
 
-    @Override
-    public void setResponse(HttpServletResponse response, AccessDeniedException ex)
-        throws IOException {
         response.setStatus(SC_UNAUTHORIZED);
         response.setContentType(APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(objectMapper.writeValueAsString(
-            ResponseData.of(ACCESS_DENIED, ex.getMessage())));
+            ResponseData.of(ACCESS_DENIED, accessDeniedException.getMessage())));
     }
 }
--- chaewsstore-core/common/src/main/java/com/chaewsstore/core/common/auth/FilterExceptionResolver.java
+++
@@ -1,9 +0,0 @@
-package com.chaewsstore.core.common.auth;
-
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public interface FilterExceptionResolver<T extends RuntimeException> {
-
-    void setResponse(HttpServletResponse response, T ex) throws IOException;
-}
