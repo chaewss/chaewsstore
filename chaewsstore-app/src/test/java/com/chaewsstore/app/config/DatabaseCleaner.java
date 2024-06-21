e5b9af4a017f284278d93722fc9c2d772d7d4dfb
chaewss <chaewseo12@gmail.com>
2024-06-21 19:07:15 +0900

    chore: DB 초기화 Bean 생성

---
+++ chaewsstore-app/src/test/java/com/chaewsstore/app/config/DatabaseCleaner.java
@@ -0,0 +1,42 @@
+package com.chaewsstore.app.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DatabaseCleaner {
+
+    private final List<String> tableNames = new ArrayList<>();
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PostConstruct
+    @SuppressWarnings("unchecked")
+    private void findDatabaseTableNames() {
+        List<Object[]> tableInfos = entityManager.createNativeQuery("SHOW TABLES").getResultList();
+        for (Object[] tableInfo : tableInfos) {
+            String tableName = (String) tableInfo[0];
+            tableNames.add(tableName);
+        }
+    }
+
+    @Transactional
+    public void clear() {
+        entityManager.clear();
+        truncate();
+    }
+
+    private void truncate() {
+        entityManager.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS %d", 0)).executeUpdate();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery(String.format("TRUNCATE TABLE %s", tableName)).executeUpdate();
+        }
+        entityManager.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS %d", 1)).executeUpdate();
+    }
+}
