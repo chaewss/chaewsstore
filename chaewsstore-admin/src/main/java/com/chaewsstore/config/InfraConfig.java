e6af2a5a3017a183930b120e436225f0f4931526
chaewss <chaewseo12@gmail.com>
2024-07-23 17:08:29 +0900

    chore: InfraConfig에 JWT 설정 추가 #22

--- chaewsstore-admin/src/main/java/com/chaewsstore/config/InfraConfig.java
+++ chaewsstore-admin/src/main/java/com/chaewsstore/config/InfraConfig.java
@@ -1,11 +1,12 @@
 package com.chaewsstore.config;
 
+import com.chaewsstore.core.infra.importer.ChaewsstoreConfigGroup;
 import com.chaewsstore.core.infra.importer.EnableChaewsstoreConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 @EnableChaewsstoreConfig({
-
+    ChaewsstoreConfigGroup.JWT,
 })
 class InfraConfig {
 
