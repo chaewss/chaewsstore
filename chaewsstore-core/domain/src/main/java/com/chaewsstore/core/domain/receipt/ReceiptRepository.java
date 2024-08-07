728aa4bd323789651a7b1ec3bc4902b490c8ddb3
chaewss <chaewseo12@gmail.com>
2024-08-07 21:30:40 +0900

    chore: receipt 삭제 #47

--- chaewsstore-core/domain/src/main/java/com/chaewsstore/core/domain/receipt/ReceiptRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chaewsstore.core.domain.receipt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface ReceiptRepository extends JpaRepository<Receipt, Long> {
-
-}
