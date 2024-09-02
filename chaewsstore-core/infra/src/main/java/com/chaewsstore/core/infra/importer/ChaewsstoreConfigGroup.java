e70a9620126e3572783290330e47b1d2251cfa4a
chaewss <chaewseo12@gmail.com>
2024-09-02 17:54:50 +0900

    chore: P6Spy 설정 추가 #56

--- chaewsstore-core/infra/src/main/java/com/chaewsstore/core/infra/importer/ChaewsstoreConfigGroup.java
+++ chaewsstore-core/infra/src/main/java/com/chaewsstore/core/infra/importer/ChaewsstoreConfigGroup.java
@@ -1,6 +1,7 @@
 package com.chaewsstore.core.infra.importer;
 
 import com.chaewsstore.core.infra.jwt.TokenProvider;
+import com.chaewsstore.core.infra.p6spy.P6SpyConfig;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 public enum ChaewsstoreConfigGroup {
 
     JWT(TokenProvider.class),
+    P6SPY(P6SpyConfig.class),
     ;
 
     private final Class<? extends ChaewsstoreConfig> configClass;
